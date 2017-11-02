b5d022f8fe4c15b512bcc5e54ed9f2679acb6e6a
Tonko Mulder <t.mulder@jvdmedia.nl>
2017-11-02 22:33:52 +0100

    added pusher import in bootstrap.js
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -38,6 +38,7 @@ window.axios.defaults.headers.common = {
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
+import Pusher from 'pusher-js'
 
 import Echo from "laravel-echo"
 
